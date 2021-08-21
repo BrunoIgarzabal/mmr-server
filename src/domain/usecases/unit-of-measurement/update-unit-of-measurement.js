5ad38e692805ebb25d0a4905ab690adedc76b85e
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 16:51:37 -0300

    feat: ensure UnitOfMeasurementRepository update is called with correct model

--- src/domain/usecases/unit-of-measurement/update-unit-of-measurement.js
+++ src/domain/usecases/unit-of-measurement/update-unit-of-measurement.js
@@ -1,6 +1,11 @@
 const { MissingParamError } = require('../../../shared/errors')
+const { UnitOfMeasurement } = require('../../models')
 
 module.exports = class UpdateUnitOfMeasurementUseCase {
+  constructor ({ unitOfMeasurementRepository } = {}) {
+    this.unitOfMeasurementRepository = unitOfMeasurementRepository
+  }
+
   async update ({ id, name, symbol } = {}) {
     if (!id) {
       throw new MissingParamError('id')
@@ -13,5 +18,8 @@ module.exports = class UpdateUnitOfMeasurementUseCase {
     if (!symbol) {
       throw new MissingParamError('symbol')
     }
+
+    const unitOfMeasurement = new UnitOfMeasurement({ id, name, symbol })
+    await this.unitOfMeasurementRepository.update(unitOfMeasurement)
   }
 }
