281ff1be59d64dce99c018f1b9a0bc6f359202f7
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 16:44:20 -0300

    test: ensure UpdateUnitOfMeasurementUseCase throws if no name is provided

--- src/domain/usecases/unit-of-measurement/update-unit-of-measurement.spec.js
+++ src/domain/usecases/unit-of-measurement/update-unit-of-measurement.spec.js
@@ -1,6 +1,8 @@
 const { MissingParamError } = require('../../../shared/errors')
 const UpdateUnitOfMeasurementUseCase = require('./update-unit-of-measurement')
 
+const anyId = 1
+
 const makeSut = () => {
   const sut = new UpdateUnitOfMeasurementUseCase()
 
@@ -13,5 +15,12 @@ describe('UpdateUnitOfMeasurementUseCase', () => {
     const promise = sut.update({ name: 'any_name', symbol: 'any_symbol' })
 
     await expect(promise).rejects.toThrow(new MissingParamError('id'))
+  })
+
+  test('should throw if no name is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.update({ id: anyId, symbol: 'any_symbol' })
+
+    await expect(promise).rejects.toThrow(new MissingParamError('name'))
   })
 })
