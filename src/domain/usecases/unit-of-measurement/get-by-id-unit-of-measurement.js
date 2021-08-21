f66322cb30210dc6e109007b7a4efe973408a669
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 20:33:32 -0300

    feat: ensure GetByIdUnitOfMeasurementUseCase returns a model if data is found

--- src/domain/usecases/unit-of-measurement/get-by-id-unit-of-measurement.js
+++ src/domain/usecases/unit-of-measurement/get-by-id-unit-of-measurement.js
@@ -1,9 +1,17 @@
 const { MissingParamError } = require('../../../shared/errors')
 
 module.exports = class GetByIdUnitOfMeasurementUseCase {
+  constructor ({ unitOfMeasurementRepository } = {}) {
+    this.unitOfMeasurementRepository = unitOfMeasurementRepository
+  }
+
   async get (id) {
     if (!id) {
       throw new MissingParamError('id')
     }
+
+    const unitOfMeasurement = await this.unitOfMeasurementRepository.getById(id)
+
+    return unitOfMeasurement
   }
 }
