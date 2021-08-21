4a37c386ab7ba9f96de2fe7cc0ddae63747f57cb
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 16:58:37 -0300

    feat: ensure UnitOfMeasurementRepository update throws if no dependency is provided

--- src/domain/usecases/unit-of-measurement/update-unit-of-measurement.js
+++ src/domain/usecases/unit-of-measurement/update-unit-of-measurement.js
@@ -1,4 +1,4 @@
-const { MissingParamError } = require('../../../shared/errors')
+const { MissingParamError, ServerError } = require('../../../shared/errors')
 const { UnitOfMeasurement } = require('../../models')
 
 module.exports = class UpdateUnitOfMeasurementUseCase {
@@ -17,6 +17,14 @@ module.exports = class UpdateUnitOfMeasurementUseCase {
 
     if (!symbol) {
       throw new MissingParamError('symbol')
+    }
+
+    if (!this.unitOfMeasurementRepository) {
+      throw new ServerError('No UnitOfMeasurementRepository provided')
+    }
+
+    if (!this.unitOfMeasurementRepository.update) {
+      throw new ServerError('No valid UnitOfMeasurementRepository provided')
     }
 
     const unitOfMeasurement = new UnitOfMeasurement({ id, name, symbol })
