1ba7512d871cca861277b3c07e7d6aceeaaea7bf
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 20:01:15 -0300

    feat: ensure UnitOfMeasurementRepository delete is called with correct id

--- src/domain/usecases/unit-of-measurement/delete-unit-of-measurement.js
+++ src/domain/usecases/unit-of-measurement/delete-unit-of-measurement.js
@@ -1,9 +1,15 @@
 const { MissingParamError } = require('../../../shared/errors')
 
 module.exports = class DeleteUnitOfMeasurementUseCase {
+  constructor ({ unitOfMeasurementRepository } = {}) {
+    this.unitOfMeasurementRepository = unitOfMeasurementRepository
+  }
+
   async delete (id) {
     if (!id) {
       throw new MissingParamError('id')
     }
+
+    await this.unitOfMeasurementRepository.delete(id)
   }
 }
