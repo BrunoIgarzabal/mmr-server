a9c1d2d6fd0d6faf496da627c4c6f9c89813f097
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 14:36:41 -0300

    feat: ensure CreateUnitOfMeasurementUseCase throws if no name is provided

---
+++ src/domain/usecases/unit-of-measurement/create-unit-of-measurement.js
@@ -0,0 +1,9 @@
+const { MissingParamError } = require('../../../shared/errors')
+
+module.exports = class CreateUnitOfMeasurementUseCase {
+  async create ({ name, symbol } = {}) {
+    if (!name) {
+      throw new MissingParamError('name')
+    }
+  }
+}
