fb22e0c3dba502cad13dc0a9cb391a1e3ee2486e
Bruno Igarzabal <brunoigar@live.com>
2021-08-21 21:01:52 -0300

    feat: ensure GetByIdUnitOfMeasurementUseCase throws if no dependency is provided

--- src/domain/usecases/unit-of-measurement/get-by-id-unit-of-measurement.js
+++ src/domain/usecases/unit-of-measurement/get-by-id-unit-of-measurement.js
@@ -1,4 +1,4 @@
-const { MissingParamError, ObjectNotFoundError } = require('../../../shared/errors')
+const { MissingParamError, ObjectNotFoundError, ServerError } = require('../../../shared/errors')
 
 module.exports = class GetByIdUnitOfMeasurementUseCase {
   constructor ({ unitOfMeasurementRepository } = {}) {
@@ -8,6 +8,14 @@ module.exports = class GetByIdUnitOfMeasurementUseCase {
   async get (id) {
     if (!id) {
       throw new MissingParamError('id')
+    }
+
+    if (!this.unitOfMeasurementRepository) {
+      throw new ServerError('No UnitOfMeasurementRepository provided')
+    }
+
+    if (!this.unitOfMeasurementRepository.getById) {
+      throw new ServerError('No valid UnitOfMeasurementRepository provided')
     }
 
     const unitOfMeasurement = await this.unitOfMeasurementRepository.getById(id)
