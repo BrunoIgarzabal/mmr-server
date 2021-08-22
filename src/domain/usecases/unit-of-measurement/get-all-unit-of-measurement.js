5a8eb61acbfc33b7404113745ddd713d9a50a6aa
Bruno Igarzabal <brunoigar@live.com>
2021-08-22 10:26:17 -0300

    feat: ensure GetAllUnitOfMeasurementUseCase throws if no dependency is provided

--- src/domain/usecases/unit-of-measurement/get-all-unit-of-measurement.js
+++ src/domain/usecases/unit-of-measurement/get-all-unit-of-measurement.js
@@ -1,9 +1,19 @@
+const { ServerError } = require('../../../shared/errors')
+
 module.exports = class GetAllUnitOfMeasurementUseCase {
   constructor ({ unitOfMeasurementRepository } = {}) {
     this.unitOfMeasurementRepository = unitOfMeasurementRepository
   }
 
   async getAll () {
+    if (!this.unitOfMeasurementRepository) {
+      throw new ServerError('No UnitOfMeasurementRepository provided')
+    }
+
+    if (!this.unitOfMeasurementRepository.getAll) {
+      throw new ServerError('No valid UnitOfMeasurementRepository provided')
+    }
+
     const list = await this.unitOfMeasurementRepository.getAll()
     return list
   }
