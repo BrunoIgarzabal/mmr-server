1ac4d390bffe4d7f5acc999006d50825d98b0f8d
Bruno Igarzabal <brunoigar@live.com>
2021-08-26 11:07:36 -0300

    feat: ensure SaveUnitOfMeasurementRepository throws if no name and symbol is provided

--- src/infra/repositories/unit-of-measurement/save-unit-of-measurement-repository.js
+++ src/infra/repositories/unit-of-measurement/save-unit-of-measurement-repository.js
@@ -1,7 +1,16 @@
+const { MissingParamError } = require('../../../shared/errors')
 const { UnitOfMeasurement } = require('../../orm/sequelize/models')
 
 module.exports = class SaveUnitOfMeasurementRepository {
   async create ({ name, symbol }) {
+    if (!name) {
+      throw new MissingParamError('name')
+    }
+
+    if (!symbol) {
+      throw new MissingParamError('symbol')
+    }
+
     const unitOfMeasurement = await UnitOfMeasurement.create({ name, symbol })
     return unitOfMeasurement
   }
