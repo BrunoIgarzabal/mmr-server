7a8ae7d73c599fad5be6f1387fae65afda145e34
Bruno Igarzabal <brunoigar@live.com>
2021-08-26 21:29:15 -0300

    test: call app listen

---
+++ tests/main/index.spec.js
@@ -0,0 +1,16 @@
+
+describe('Index', () => {
+  test('should call app listen', () => {
+    jest.mock('../../src/main/config/app', () => ({
+      listen (port, callback) {
+        if (callback) {
+          callback()
+        }
+      }
+    }))
+    const mock = jest.requireMock('../../src/main/config/app')
+    const listen = jest.spyOn(mock, 'listen')
+    require('../../src/main/index')
+    expect(listen).toHaveBeenCalledTimes(1)
+  })
+})
