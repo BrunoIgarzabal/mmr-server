a70170118c4901511f68f7b6d405f9c3f3e8a9b7
Bruno Igarzabal <brunoigar@live.com>
2021-08-26 21:03:07 -0300

    feat: ensure requests return content-type as json by default

---
+++ tests/main/middlewares/content-type.test.js
@@ -0,0 +1,15 @@
+const app = require('../../../src/main/config/app')
+
+const request = require('supertest')
+
+describe('Content-Type Middleware', () => {
+  test('should return json content-type as default', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+})
