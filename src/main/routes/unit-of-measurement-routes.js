5860ffb32ba934277fa00da78bed018b0a8baee4
Bruno Igarzabal <brunoigar@live.com>
2021-08-26 22:11:18 -0300

    feat: add express router adapter

--- src/main/routes/unit-of-measurement-routes.js
+++ src/main/routes/unit-of-measurement-routes.js
@@ -1,5 +1,7 @@
+const ExpressRouterAdapter = require('../adapters/express-router-adapter')
+
 const { createUnitOfMeasurementRouter } = require('../composers/unit-of-measurement')
 
 module.exports = router => {
-  router.post('/unit-of-measurement', createUnitOfMeasurementRouter)
+  router.post('/unit-of-measurement', ExpressRouterAdapter.adapt(createUnitOfMeasurementRouter))
 }
