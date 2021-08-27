9460ea3f84d7472057ffa504e9c5b1826fc63dee
Bruno Igarzabal <brunoigar@live.com>
2021-08-26 21:41:21 -0300

    refactor: remove main to tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.js', '!**/src/main/**'],
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   coverageProvider: 'v8'
--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,10 @@
 require('dotenv/config')
-const app = require('./config/app')
+const connection = require('../infra/orm/sequelize/sequelize')
 
-app.listen(process.env.SERVER_PORT, () => console.log(`Server Running on port ${process.env.SERVER_PORT}`))
+connection.sync()
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(process.env.SERVER_PORT,
+      () => console.log('🌎 Server Running 🛰️'))
+  })
+  .catch(console.error)
